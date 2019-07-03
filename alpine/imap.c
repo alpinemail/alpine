bc305b03f09ec01a768241d8ed8e740534caa60e
Doug Luce <doug@github.con.com>
2019-07-03 15:56:41 -0700

    Fix compilation on MacOS
    Although it's not entirely clear to me the substantive difference
    between LOCAL_PASSWD_CACHE and PASSFILE

--- alpine/imap.c
+++ alpine/imap.c
@@ -591,6 +591,7 @@ mm_login_oauth2(NETMBX *mb, char *user, OAUTH2_S *login, long int trial,
 		      altuserforcache ? altuserforcache : user, hostlist,
 		      (mb->sslflag||mb->tlsflag), 0, 0, OA2NAME);
 #ifdef	LOCAL_PASSWD_CACHE
+#ifdef	PASSFILE
     /* if requested, remember it on disk for next session */
     if(save_password && F_OFF(F_DISABLE_PASSWORD_FILE_SAVING,ps_global))
 	    set_passfile_passwd_auth(ps_global->pinerc, token,
@@ -598,6 +599,7 @@ mm_login_oauth2(NETMBX *mb, char *user, OAUTH2_S *login, long int trial,
 			(mb->sslflag||mb->tlsflag),
 			(preserve_password == -1 ? 0
 			 : (preserve_password == 0 ? 2 :1)), OA2NAME);
+#endif	/* PASSFILE */
 #endif	/* LOCAL_PASSWD_CACHE */
 
     ps_global->no_newmail_check_from_optionally_enter = 0;
@@ -1495,6 +1497,7 @@ mm_login_work(NETMBX *mb, char *user, char **pwd, long int trial,
 		      altuserforcache ? altuserforcache : user, hostlist,
 		      (mb->sslflag||mb->tlsflag), 0, 0);
 #ifdef	LOCAL_PASSWD_CACHE
+#ifdef  PASSFILE
     /* if requested, remember it on disk for next session */
       if(save_password && F_OFF(F_DISABLE_PASSWORD_FILE_SAVING,ps_global))
       set_passfile_passwd(ps_global->pinerc, *pwd,
@@ -1502,6 +1505,7 @@ mm_login_work(NETMBX *mb, char *user, char **pwd, long int trial,
 			(mb->sslflag||mb->tlsflag),
 			(preserve_password == -1 ? 0
 			 : (preserve_password == 0 ? 2 :1)));
+#endif	/* PASSFILE */
 #endif	/* LOCAL_PASSWD_CACHE */
 
     ps_global->no_newmail_check_from_optionally_enter = 0;
