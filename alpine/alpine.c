alpinemail/alpine

commit 8961761e0b3c7b3cc11a00f6ac6ebf7a29bc5a10

* Fix some memory leaks reported by Valgrind.

alpine/alpine.c
@@ -65,6 +65,7 @@ static char rcsid[] = "$Id: alpine.c 1266 2009-07-14 18:39:12Z hubert@u.washingt
 #include "smime.h"
 #include "newmail.h"
 #include "xoauth2conf.h"
+#include "xoauth2info.h"
 #ifndef _WINDOWS
 #include "../pico/osdep/raw.h"	/* for STD*_FD */
 #endif
@@ -692,6 +693,9 @@ main(int argc, char **argv)
 
 	   min_v = pith_ssl_encryption_version(min_s);
 	   max_v = pith_ssl_encryption_version(max_s);
+
+	   if(min_s != NULL) fs_give((void **) &min_s);
+	   if(max_s != NULL) fs_give((void **) &max_s);
 
 	   if(min_v < 0 || max_v < 0){
 	      snprintf(tmp_20k_buf, SIZEOF_20KBUF,
@@ -3405,6 +3409,7 @@ goodnight_gracey(struct pine *pine_state, int exit_val)
     free_pith_module_globals();
     free_pico_module_globals();
     free_c_client_module_globals();
+    xoauth_free_info();
 
 #ifdef DEBUG
     if(debugfile){
@@ -3653,10 +3658,7 @@ prune_folders_ok(void)
 
 void  
 free_alpine_module_globals(void)
-{   
-#ifdef  LOCAL_PASSWD_CACHE
-    free_passfile_cache();
-#endif
+{
     free_message_queue();
     free_titlebar_globals();
 }

alpine/imap.c
@@ -116,9 +116,7 @@ int   preserve_prompt(char *);
 int   preserve_prompt_auth(char *, char *authtype);
 void  update_passfile_hostlist(char *, char *, STRLIST_S *, int);
 void  update_passfile_hostlist_auth(char *, char *, STRLIST_S *, int, char *);
-void  free_passfile_cache_work(MMLOGIN_S **);
 
-static	MMLOGIN_S	*passfile_cache = NULL;
 static  int             using_passfile = -1;
 int  save_password = 1;
 #endif	/* LOCAL_PASSWD_CACHE */
@@ -875,6 +873,7 @@ mm_login_oauth2(NETMBX *mb, char *user, char *method,
 		}
 	    }
 	}
+	if (x) free_xoauth2_info(&x);
 	/* else use the one we found earlier, the user has to configure this better */
     }
 
@@ -901,6 +900,9 @@ mm_login_oauth2(NETMBX *mb, char *user, char *method,
 	ps_global->id = set_alpine_id(oa2list->app_id ? oa2list->app_id : PACKAGE_NAME, PACKAGE_VERSION);
 	mail_parameters(NULL, SET_IDPARAMS, (void *) ps_global->id);
     }
+
+    if(registered)
+       oa2list->param[OA2_State].value = login->param[OA2_State].value;
 
     /*
      * We check if we have a refresh token saved somewhere, if so
@@ -1063,6 +1065,7 @@ mm_login_oauth2(NETMBX *mb, char *user, char *method,
        oa2list->cancel_refresh_token = login->cancel_refresh_token;
        *login = *oa2list;	/* load login pointer */
     }
+    if(token) fs_give((void **) &token);
 
     if(!ChangeAccessToken && !ChangeRefreshToken && !login->cancel_refresh_token)
        return;
@@ -1071,7 +1074,6 @@ mm_login_oauth2(NETMBX *mb, char *user, char *method,
      *  RefreshToken \001 LastAccessToken \001 ExpirationTime
      * (spaces added for clarity, \001 is PWDAUTHSEP)
      */
-    if(token) fs_give((void **) &token);
     sprintf(tmp, "%lu", SaveExpirationTime);
     tmp[sizeof(tmp) - 1] = '\0';
     len = strlen(SaveRefreshToken ? SaveRefreshToken : "")
@@ -3943,31 +3945,10 @@ get_passfile_passwd_auth(pinerc, passwd, user, hostlist, altflag, authtype)
     char      *authtype;
 {
     dprint((10, "get_passfile_passwd_auth\n"));
-    return((passfile_cache || read_passfile(pinerc, &passfile_cache))
-	     ? imap_get_passwd_auth(passfile_cache, passwd,
+    return((mm_login_list || read_passfile(pinerc, &mm_login_list))
+	     ? imap_get_passwd_auth(mm_login_list, passwd,
 			       user, hostlist, altflag, authtype)
 	     : 0);
-}
-
-void
-free_passfile_cache_work(MMLOGIN_S **pwdcache)
-{
-  if(pwdcache == NULL || *pwdcache == NULL)
-    return;
-
-  if((*pwdcache)->user) fs_give((void **)&(*pwdcache)->user);
-//  if((*pwdcache)->passwd) fs_give((void **)&(*pwdcache)->passwd);
-  if((*pwdcache)->hosts) free_strlist(&(*pwdcache)->hosts);
-  free_passfile_cache_work(&(*pwdcache)->next);
-  fs_give((void **)pwdcache);
-}
-
-
-void
-free_passfile_cache(void)
-{
-  if(passfile_cache)
-    free_passfile_cache_work(&passfile_cache);
 }
 
 int
@@ -3985,8 +3966,8 @@ get_passfile_user(pinerc, hostlist)
     char      *pinerc;
     STRLIST_S *hostlist;
 {
-    return((passfile_cache || read_passfile(pinerc, &passfile_cache))
-	     ? imap_get_user(passfile_cache, hostlist)
+    return((mm_login_list || read_passfile(pinerc, &mm_login_list))
+	     ? imap_get_user(mm_login_list, hostlist)
 	     : NULL);
 }
 
@@ -4216,9 +4197,9 @@ set_passfile_passwd_auth(pinerc, passwd, user, hostlist, altflag, already_prompt
     if(((already_prompted == 0 && preserve_prompt_auth(pinerc, authtype))
 	   || already_prompted == 1)
        && !ps_global->nowrite_password_cache
-       && (passfile_cache || read_passfile(pinerc, &passfile_cache))){
-	imap_set_passwd_auth(&passfile_cache, passwd, user, hostlist, altflag, 0, 0, authtype);
-	write_passfile(pinerc, passfile_cache);
+       && (mm_login_list || read_passfile(pinerc, &mm_login_list))){
+	imap_set_passwd_auth(&mm_login_list, passwd, user, hostlist, altflag, 0, 0, authtype);
+	write_passfile(pinerc, mm_login_list);
     }
 }
 
@@ -4255,7 +4236,7 @@ update_passfile_hostlist_auth(pinerc, user, hostlist, altflag, authtype)
     size_t  len    = authtype ? strlen(authtype) : 0;
     size_t  offset = authtype ? 1 : 0;
     
-    for(l = passfile_cache; l; l = l->next)
+    for(l = mm_login_list; l; l = l->next)
       if(imap_same_host_auth(l->hosts, hostlist, authtype)
 	 && *user
 	 && !strcmp(user, l->user + len + offset)
@@ -4267,7 +4248,7 @@ update_passfile_hostlist_auth(pinerc, user, hostlist, altflag, authtype)
        && hostlist->next->name
        && !ps_global->nowrite_password_cache){
 	l->hosts->next = new_strlist_auth(hostlist->next->name, authtype, PWDAUTHSEP);
-	write_passfile(pinerc, passfile_cache);
+	write_passfile(pinerc, mm_login_list);
     }
 #endif /* !WINCRED */
 }

alpine/xoauth2info.c
@@ -223,3 +223,16 @@ OAUTH2_S alpine_oauth2_list[] = {
    0					/* flags */
   }
 };
+
+
+void
+xoauth_free_info(void)
+{
+  int i;
+  for(i = 0; alpine_oauth2_list[i].name != NULL ; i++){
+     if(alpine_oauth2_list[i].param[OA2_RefreshToken].value)
+	fs_give((void **) &alpine_oauth2_list[i].param[OA2_RefreshToken].value);
+     if(alpine_oauth2_list[i].access_token)
+	fs_give((void **) &alpine_oauth2_list[i].access_token);
+  }
+}

imap/src/c-client/auth_oa2.c
@@ -174,5 +174,8 @@ long auth_oauth2_client (authchallenge_t challenger,authrespond_t responder, cha
   if(oauth2.param[OA2_Id].value) fs_give((void **) &oauth2.param[OA2_Id].value);
   if(oauth2.param[OA2_Secret].value) fs_give((void **) &oauth2.param[OA2_Secret].value);
   if(oauth2.param[OA2_Tenant].value) fs_give((void **) &oauth2.param[OA2_Tenant].value);
+  if(oauth2.param[OA2_State].value) fs_give((void **) &oauth2.param[OA2_State].value);
+  if(oauth2.param[OA2_RefreshToken].value) fs_give((void **) &oauth2.param[OA2_RefreshToken].value);
+  if(oauth2.access_token) fs_give((void **) &oauth2.access_token);
   return ret;
 }

imap/src/c-client/oauth2_aux.c
@@ -425,8 +425,7 @@ void renew_accesstoken(MAILSTREAM *stream)
     user[0] = '\0';
     mm_login_method (&mb, user, (void *) &oauth2, trial, stream->auth.name);
 
-    if(oauth2.param[OA2_State].value)
-      fs_give((void **) &oauth2.param[OA2_State].value);
+    oauth2.param[OA2_State].value = NIL; /* this is freed before we get here */
 
     if(stream->auth.expiration == 0){
        stream->auth.expiration = oauth2.expiration;
@@ -447,4 +446,7 @@ void renew_accesstoken(MAILSTREAM *stream)
     if(oauth2.param[OA2_Id].value) fs_give((void **) &oauth2.param[OA2_Id].value);
     if(oauth2.param[OA2_Secret].value) fs_give((void **) &oauth2.param[OA2_Secret].value);
     if(oauth2.param[OA2_Tenant].value) fs_give((void **) &oauth2.param[OA2_Tenant].value);
+    if(oauth2.param[OA2_State].value) fs_give((void **) &oauth2.param[OA2_State].value);
+    if(oauth2.param[OA2_RefreshToken].value) fs_give((void **) &oauth2.param[OA2_RefreshToken].value);
+    if(oauth2.access_token) fs_give((void **) &oauth2.access_token);
 }

pith/pine.hlp
@@ -140,7 +140,7 @@ with help text for the config screen and the composer that didn't have any
 reasonable place to be called from.
 Dummy change to get revision in pine.hlp
 ============= h_revision =================
-Alpine Commit 578 2021-08-21 15:09:38
+Alpine Commit 579 2021-08-23 21:16:41
 ============= h_news =================
 <HTML>
 <HEAD>

pith/imap.c
@@ -1125,6 +1125,10 @@ imap_flush_passwd_cache(int dumpcache)
 	    if(l->user)
 	      fs_give((void **) &l->user);
 
+	    if(!(l->passwd >= (char *) private_store
+		 && l->passwd <= p))
+	      fs_give((void **) &l->passwd);
+
 	    free_strlist(&l->hosts);
 
 	    fs_give((void **) &l);
@@ -1134,6 +1138,9 @@ imap_flush_passwd_cache(int dumpcache)
 	    cert_failure_list = cert_failure_list->next;
 	    if(l->user)
 	      fs_give((void **) &l->user);
+	    if(!(l->passwd >= (char *) private_store
+		 && l->passwd <= p))
+	      fs_give((void **) &l->passwd);
 
 	    free_strlist(&l->hosts);
 

alpine/imap.h
@@ -44,7 +44,6 @@ int     is_using_passfile(void);
 void    set_passfile_passwd(char *, char *, char *, STRLIST_S *, int, int);
 void    set_passfile_passwd_auth(char *, char *, char *, STRLIST_S *, int, int, char *);
 char   *get_passfile_user(char *, STRLIST_S *);
-void	free_passfile_cache(void);
 #endif	/* LOCAL_PASSWD_CACHE */
 
 #if	(WINCRED > 0)

commit 5431d789f6a736924230ff3e51c14e0db441bdb6

* Fix of more memory leaks and a crash due to incorrect freeing of memory, introduced    in commit 8961761e0b3c7b3cc11a00f6ac6ebf7a29bc5a10

alpine/alpine.c
@@ -64,7 +64,6 @@ static char rcsid[] = "$Id: alpine.c 1266 2009-07-14 18:39:12Z hubert@u.washingt
 #include "after.h"
 #include "smime.h"
 #include "newmail.h"
-#include "xoauth2info.h"
 #ifndef _WINDOWS
 #include "../pico/osdep/raw.h"	/* for STD*_FD */
 #endif
@@ -3408,7 +3407,6 @@ goodnight_gracey(struct pine *pine_state, int exit_val)
     free_pith_module_globals();
     free_pico_module_globals();
     free_c_client_module_globals();
-    xoauth_free_info();
 
 #ifdef DEBUG
     if(debugfile){

alpine/imap.c
@@ -1046,6 +1046,7 @@ mm_login_oauth2(NETMBX *mb, char *user, char *method,
 	&& (NewAccessToken == NULL || strcmp(OldAccessToken, NewAccessToken))){
 	if(NewAccessToken) fs_give((void **) &NewAccessToken);
 	NewAccessToken = cpystr(OldAccessToken);
+	NewAccessToken = OldAccessToken;
 	ChangeAccessToken++;
 	NewExpirationTime = OldExpirationTime;
 	SaveRefreshToken = NewRefreshToken;
@@ -1099,7 +1100,7 @@ mm_login_oauth2(NETMBX *mb, char *user, char *method,
 			(preserve_password == -1 ? 0
 			 : (preserve_password == 0 ? 2 :1)), OA2NAME);
 #endif	/* LOCAL_PASSWD_CACHE */
-
+    if (token) fs_give((void **) &token);
     ps_global->no_newmail_check_from_optionally_enter = 0;
 }
 

alpine/xoauth2info.c
@@ -223,16 +223,3 @@ OAUTH2_S alpine_oauth2_list[] = {
    0					/* flags */
   }
 };
-
-
-void
-xoauth_free_info(void)
-{
-  int i;
-  for(i = 0; alpine_oauth2_list[i].name != NULL ; i++){
-     if(alpine_oauth2_list[i].param[OA2_RefreshToken].value)
-	fs_give((void **) &alpine_oauth2_list[i].param[OA2_RefreshToken].value);
-     if(alpine_oauth2_list[i].access_token)
-	fs_give((void **) &alpine_oauth2_list[i].access_token);
-  }
-}

alpine/xoauth2info.h
@@ -1,20 +1,0 @@
-/*
- * ========================================================================
- * Copyright 2013-2021 Eduardo Chappa
- * Copyright 2006-2009 University of Washington
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * ========================================================================
- */
-
-#ifndef XOAUTH2INFO_INCLUDED
-#define XOAUTH2INFO_INCLUDED
-
-void xoauth_free_info(void);
-
-#endif /* XOAUTH2INFO_INCLUDED */

imap/src/c-client/auth_oa2.c
@@ -25,7 +25,9 @@ AUTHENTICATOR auth_oa2 = {
 };
 
 #define OAUTH2_USER	"user="
+#define OAUTH2_USER_LEN  (5)	/* strlen(OAUTH2_USER) */
 #define OAUTH2_BEARER	"auth=Bearer "
+#define OAUTH2_BEARER_LEN  (12)	/* strlen(OAUTH2_BEARER) */
 
 /* Client authenticator
  * Accepts: challenger function
@@ -142,8 +144,8 @@ long auth_oauth2_client (authchallenge_t challenger,authrespond_t responder, cha
       ret = base ? NIL : LONGT;		/* will get a BAD response back */
     }
     else {
-      unsigned long rlen = strlen(OAUTH2_USER) + strlen(user)
-			+ strlen(OAUTH2_BEARER) + strlen(oauth2.access_token) + 1 + 2;
+      unsigned long rlen = OAUTH2_USER_LEN + OAUTH2_BEARER_LEN + 2
+			   + strlen(user) + strlen(oauth2.access_token) + 1;
       char *response = (char *) fs_get (rlen + 1);
       sprintf(response, "%s%s\001%s%s\001\001", OAUTH2_USER, user, OAUTH2_BEARER, oauth2.access_token);
       if ((*responder) (stream,base,response,rlen)) {
@@ -157,13 +159,8 @@ long auth_oauth2_client (authchallenge_t challenger,authrespond_t responder, cha
 	   * Refresh Token has expired somehow, we invalidate it if we
 	   * reach *trial to 3. This forces the process to restart later on.
 	   */
-	  if(*trial == 3){
-	     if(oauth2.param[OA2_State].value)
-		fs_give((void **) &oauth2.param[OA2_State].value);
-	     fs_give((void **) &oauth2.param[OA2_RefreshToken].value);
-	     fs_give((void **) &oauth2.access_token);
+	  if(*trial == 3)
 	     oauth2.expiration = 0L;
-	  }
 	}
       }
       fs_give ((void **) &response);

imap/src/c-client/oauth2_aux.c
@@ -143,6 +143,7 @@ mm_login_oauth2_c_client_method (NETMBX *mb, char *user, char *method,
      if(ogci && (x = (*ogci)(oauth2->name, user)) != NULL){
 	 oauth2->param[OA2_Id].value = cpystr(x->client_id);
 	 oauth2->param[OA2_Secret].value = x->client_secret ? cpystr(x->client_secret) : NULL;
+	 if(oauth2->param[OA2_Tenant].value) fs_give((void **) &oauth2->param[OA2_Tenant].value);
 	 oauth2->param[OA2_Tenant].value = x->tenant ? cpystr(x->tenant) : NULL;
 	 free_xoauth2_info(&x);
      }
@@ -207,7 +208,9 @@ mm_login_oauth2_c_client_method (NETMBX *mb, char *user, char *method,
 	   case HTTP_UNAUTHORIZED:
 			mm_log("Client not authorized (wrong client-id?)", ERROR);
 			break;
-	   case HTTP_OK: json_assign ((void **) &oauth2->access_token, json, "access_token", JString);
+	   case HTTP_OK: if(oauth2->access_token)
+			    fs_give((void **) &oauth2->access_token);
+			 json_assign ((void **) &oauth2->access_token, json, "access_token", JString);
 			 if((jx = json_body_value(json, "expires_in")) != NULL)
 			 switch(jx->jtype){
 			      case JString: oauth2->expiration = time(0) + atol((char *) jx->value);
@@ -266,7 +269,11 @@ mm_login_oauth2_c_client_method (NETMBX *mb, char *user, char *method,
 	   JSON_S *jx;
 
 	  switch(status){
-	     case HTTP_OK : json_assign ((void **) &oauth2->param[OA2_RefreshToken].value, json, "refresh_token", JString);
+	     case HTTP_OK : if(oauth2->param[OA2_RefreshToken].value)
+			       fs_give((void **) &oauth2->param[OA2_RefreshToken].value);
+			    json_assign ((void **) &oauth2->param[OA2_RefreshToken].value, json, "refresh_token", JString);
+			    if(oauth2->access_token)
+			       fs_give((void **) &oauth2->access_token);
 			    json_assign ((void **) &oauth2->access_token, json, "access_token", JString);
 
 			    if((jx = json_body_value(json, "expires_in")) != NULL)
@@ -339,8 +346,12 @@ void oauth2deviceinfo_get_accesscode(void *inp, void *outp)
 
 			break;
 
-	case HTTP_OK :  json_assign ((void **) &oauth2->param[OA2_RefreshToken].value, json, "refresh_token", JString);
-			json_assign ((void **) &oauth2->access_token, json, "access_token", JString);
+	case HTTP_OK :   if(oauth2->param[OA2_RefreshToken].value)
+			   fs_give((void **) &oauth2->param[OA2_RefreshToken].value);
+			 json_assign ((void **) &oauth2->param[OA2_RefreshToken].value, json, "refresh_token", JString);
+			 if(oauth2->access_token)
+			       fs_give((void **) &oauth2->access_token);
+			 json_assign ((void **) &oauth2->access_token, json, "access_token", JString);
 
 			if((jx = json_body_value(json, "expires_in")) != NULL)
 			  switch(jx->jtype){
@@ -425,15 +436,14 @@ void renew_accesstoken(MAILSTREAM *stream)
     user[0] = '\0';
     mm_login_method (&mb, user, (void *) &oauth2, trial, stream->auth.name);
 
-    oauth2.param[OA2_State].value = NIL; /* this is freed before we get here */
+    if(oauth2.access_token)	/* we need a new one */
+      fs_give((void **) &oauth2.access_token);
 
     if(stream->auth.expiration == 0){
        stream->auth.expiration = oauth2.expiration;
+       if(oauth2.param[OA2_RefreshToken].value) fs_give((void **) &oauth2.param[OA2_RefreshToken].value);
        return;
     }
-
-    if(oauth2.access_token)
-      fs_give((void **) &oauth2.access_token);
 
     oauth2.param[OA2_State].value = oauth2_generate_state();
 

pith/pine.hlp
@@ -140,7 +140,7 @@ with help text for the config screen and the composer that didn't have any
 reasonable place to be called from.
 Dummy change to get revision in pine.hlp
 ============= h_revision =================
-Alpine Commit 581 2021-08-23 22:42:05
+Alpine Commit 582 2021-08-28 00:40:42
 ============= h_news =================
 <HTML>
 <HEAD>