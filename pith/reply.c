alpinemail/alpine

commit 8167f58e118afa658590253f8696c816511c3708

* Clear more warnings given by gcc-10. Work in progress.

pith/pine.hlp
@@ -140,7 +140,7 @@ with help text for the config screen and the composer that didn't have any
 reasonable place to be called from.
 Dummy change to get revision in pine.hlp
 ============= h_revision =================
-Alpine Commit 568 2021-07-30 09:00:01
+Alpine Commit 569 2021-07-31 21:16:12
 ============= h_news =================
 <HTML>
 <HEAD>

pith/reply.c
@@ -2331,10 +2331,11 @@ forward_subject(ENVELOPE *env, int flags)
 	removing_trailing_white_space(tmp_20k_buf);
 	if((l = strlen(tmp_20k_buf)) < 1000 &&
 	   (l < 5 || strcmp(tmp_20k_buf+l-5,"(fwd)"))){
-	    snprintf(tmp_20k_buf+2000, SIZEOF_20KBUF-2000, "%s (fwd)", tmp_20k_buf);
-	    tmp_20k_buf[SIZEOF_20KBUF-2000-1] = '\0';
-	    memmove(tmp_20k_buf, tmp_20k_buf+2000, strlen(tmp_20k_buf+2000));
-	    tmp_20k_buf[strlen(tmp_20k_buf+2000)] = '\0';
+	    char *s = cpystr(tmp_20k_buf);
+	    snprintf(tmp_20k_buf, SIZEOF_20KBUF, "%.1000s (fwd)", s);
+	    tmp_20k_buf[SIZEOF_20KBUF-1] = '\0';
+	    strcpy(tmp_20k_buf, s);
+	    fs_give((void **) &s);
 	}
 
 	/*

alpine/imap.c
@@ -3788,11 +3788,12 @@ write_passfile(pinerc, l)
     }
 
 #else /* PASSFILE */
-    char  tmp[10*MAILTMPLEN], blob[10*MAILTMPLEN];
+    char  *tmp = NULL, passfile[MAXPATH + 1], blob[MAILTMPLEN];
     int   i, n;
+    size_t tmplen = 0, newlen;
     FILE *fp;
 #ifdef SMIME
-    char *text = NULL, tmp2[10*MAILTMPLEN];
+    char *text = NULL, tmp2[MAXPATH + 1];
     int len = 0;
 #endif
 
@@ -3802,13 +3803,13 @@ write_passfile(pinerc, l)
     dprint((9, "write_passfile\n"));
 
     /* if there's no passfile to read, bag it!! */
-    if(!passfile_name(pinerc, tmp, sizeof(tmp)) || !(fp = our_fopen(tmp, "wb"))){
+    if(!passfile_name(pinerc, passfile, sizeof(tmp)) || !(fp = our_fopen(passfile, "wb"))){
 	using_passfile = 0;
         return;
     }
 
 #ifdef SMIME
-   strncpy(tmp2, tmp, sizeof(tmp2));
+   strncpy(tmp2, passfile, sizeof(tmp2));
    tmp2[sizeof(tmp2)-1] = '\0';
 #endif /* SMIME */
 
@@ -3823,8 +3824,18 @@ write_passfile(pinerc, l)
 	else
 	   sprintf(blob, "%d", l->altflag);
 
+	newlen = strlen(l->passwd) + strlen(l->user) + strlen(l->hosts->name)
+		+ strlen(blob) + strlen((l->hosts->next && l->hosts->next->name) ? "\t" : "")
+		+ strlen((l->hosts->next && l->hosts->next->name) ? l->hosts->next->name : "")
+		+ 4 + 1;
+
+	if(tmplen < newlen){
+	   fs_resize((void **)&tmp, newlen);
+	   tmplen = newlen;
+	}
+	
 	/*** do any necessary ENcryption here ***/
-	snprintf(tmp, sizeof(tmp), "%s\t%s\t%s\t%s%s%s\n", l->passwd, l->user,
+	sprintf(tmp, "%s\t%s\t%s\t%s%s%s\n", l->passwd, l->user,
 		l->hosts->name, blob,
 		(l->hosts->next && l->hosts->next->name) ? "\t" : "",
 		(l->hosts->next && l->hosts->next->name) ? l->hosts->next->name
@@ -3844,6 +3855,7 @@ write_passfile(pinerc, l)
 #endif /* SMIME */
     }
 
+    if(tmp) fs_give((void **) &tmp);
     fclose(fp);
 #ifdef SMIME
     if(text != NULL){

alpine/ldapconf.c
@@ -1805,7 +1805,7 @@ dir_edit_screen(struct pine *ps, LDAP_SERV_S *def, char *title, char **raw_serve
      */
 
     if(rv == 1 && raw_server){
-	char dir_tmp[2200], *p;
+	char dir_tmp[2200+MAXPATH], *p;
 	int portval = -1, timeval = -1, sizeval = -1;
 
 	apval = APVAL(&server_var, ew);

alpine/mailcmd.c
@@ -3866,7 +3866,7 @@ cmd_export(struct pine *state, MSGNO_S *msgmap, int qline, int aopt)
 	    }
 
 	    ok = 0;
-	    snprintf(dir, sizeof(dir), "%s.d", full_filename);
+	    snprintf(dir, sizeof(dir), "%.*s.d", MAXPATH-2, full_filename);
 	    dir[sizeof(dir)-1] = '\0';
 
 	    do {
@@ -3887,7 +3887,7 @@ cmd_export(struct pine *state, MSGNO_S *msgmap, int qline, int aopt)
 			goto fini;
 		    }
 
-		    snprintf(dir, sizeof(dir), "%s.d_%s", full_filename,
+		    snprintf(dir, sizeof(dir), "%.*s.d_%s", MAXPATH- (int) strlen(long2string((long) tries))-3, full_filename,
 			    long2string((long) tries));
 		    dir[sizeof(dir)-1] = '\0';
 		    break;
@@ -3960,24 +3960,33 @@ cmd_export(struct pine *state, MSGNO_S *msgmap, int qline, int aopt)
 		 * and if so, we write a counter name in the file name, just before the
 		 * extension of the file, and separate it with an underscore.
 		 */
-		snprintf(filename, sizeof(filename), "%s%s%s", dir, S_FILESEP, lfile);
+		snprintf(filename, sizeof(filename), "%.*s%.*s%.*s", (int) strlen(dir), dir, 
+			(int) strlen(S_FILESEP), S_FILESEP, 
+			MAXPATH - (int) strlen(dir) - (int) strlen(S_FILESEP), lfile);
 		filename[sizeof(filename)-1] = '\0';
 		while((ok = can_access(filename, ACCESS_EXISTS)) == 0 && errs == 0){
-		   char *ext;
-		   snprintf(filename, sizeof(filename), "%d", counter);
-		   if(strlen(dir) + strlen(S_FILESEP) + strlen(lfile) + strlen(filename) + 2
-							    > sizeof(filename)){
+		   char *ext, count[MAXPATH+1];
+		   unsigned long total;
+		   snprintf(count, sizeof(count), "%d", counter);
+		   if((ext = strrchr(lfile, '.')) != NULL)
+		      *ext = '\0';
+		   total = strlen(dir) + strlen(S_FILESEP) + strlen(lfile) + strlen(count) + 3
+				+ (ext ? strlen(ext+1) : 0);
+		   if(total > sizeof(filename)){
 		      dprint((2,
 			   "FAILED Att Export: name too long: %s\n",
 			   dir, S_FILESEP, lfile));
 		      errs++;
 		      continue;
 		   }
-		   if((ext = strrchr(lfile, '.')) != NULL)
-		      *ext = '\0';
-		   snprintf(filename, sizeof(filename), "%s%s%s%s%d%s%s", 
-			dir, S_FILESEP, lfile, 
-			ext ? "_" : "", counter++, ext ? "." : "", ext ? ext+1 : "");
+		   snprintf(filename, sizeof(filename), "%.*s%.*s%.*s%.*s%.*d%.*s%.*s", 
+			(int) strlen(dir), dir, (int) strlen(S_FILESEP), S_FILESEP, 
+			(int) strlen(lfile), lfile,
+			ext ? 1 : 0, ext ? "_" : "",
+			(int) strlen(count), counter++,
+			ext ? 1 : 0, ext ? "." : "", 
+			ext ? (int) (sizeof(filename) - total) : 0,
+			ext ? ext+1 : "");
 		   filename[sizeof(filename)-1] = '\0';
 		}
 

alpine/mailpart.c
@@ -2169,7 +2169,7 @@ display_attachment(long int msgno, ATTACH_S *a, int flags)
     gf_io_t  pc;
     char    *err;
     int      we_cancel = 0, rv;
-    char     prefix[70];
+    char     prefix[70 + 1000];	/* 1000 = sizeof(sender_filename) */
     char     ext[32];
     char     mtype[128];
 

alpine/roleconf.c
@@ -3402,8 +3402,7 @@ role_config_edit_screen(struct pine *ps, PAT_S *def, char *title, long int rflag
     rolecolor_vars[1].is_used    = 1;
     rolecolor_vars[1].is_user    = 1;
     rolecolor_vars[0].name = cpystr("ic-foreground-color");
-    rolecolor_vars[1].name = cpystr(rolecolor_vars[0].name);
-    strncpy(rolecolor_vars[1].name + 3, "back", 4);
+    rolecolor_vars[1].name = cpystr("ic-background-color");
     apval = APVAL(&rolecolor_vars[1], ew);
     *apval = (def && def->action && def->action->incol &&
 	      def->action->incol->bg[0])

alpine/smime.c
@@ -1288,24 +1288,31 @@ manage_certs_tool(struct pine *ps, int cmd, CONF_S **cl, unsigned flags)
 		     && can_access(pathdir, ACCESS_EXISTS) != 0
 	             && our_mkpath(pathdir, 0700) == 0)){
 	        pc = ALPINE_self_signed_certificate(NULL, 0, pathdir, MASTERNAME);
-		snprintf(filename, sizeof(filename), "%s/%s.key", 
-					pathdir, MASTERNAME);
-		filename[sizeof(filename)-1] = '\0';
-		rv = import_certificate(ctype, pc, filename);
-		if(rv == 1){
-		  ps->keyemptypwd = 0;
-		  if(our_stat(pathdir, &sbuf) == 0){
-		    if(unlink(filename) < 0)
-		      q_status_message1(SM_ORDER, 0, 2, 
-			_("Could not remove private key %s.key"), MASTERNAME);
-		    filename[strlen(filename)-4] = '\0';
-		    strcat(filename, ".crt");
-		    if(unlink(filename) < 0)
-		      q_status_message1(SM_ORDER, 0, 2, 
-			_("Could not remove public certificate %s.crt"), MASTERNAME);
-		    if(rmdir(pathdir) < 0)
-		      q_status_message1(SM_ORDER, 0, 2, 
-		      _("Could not remove temporary directory %s"), pathdir);
+		if(strlen(pathdir) + strlen(MASTERNAME) + 5 + 1 > sizeof(filename)){
+		      q_status_message(SM_ORDER, 0, 2, 
+			_("pathdir for key too long"));
+		}
+		else{
+		  snprintf(filename, sizeof(filename), "%.*s/%.*s.key",
+				(int) strlen(pathdir), pathdir,
+				(int) (sizeof(filename) - strlen(MASTERNAME) - 5 - 1), MASTERNAME);
+		  filename[sizeof(filename)-1] = '\0';
+		  rv = import_certificate(ctype, pc, filename);
+		  if(rv == 1){
+		    ps->keyemptypwd = 0;
+		    if(our_stat(pathdir, &sbuf) == 0){
+		      if(unlink(filename) < 0)
+		        q_status_message1(SM_ORDER, 0, 2, 
+			  _("Could not remove private key %s.key"), MASTERNAME);
+		      filename[strlen(filename)-4] = '\0';
+		      strcat(filename, ".crt");
+		      if(unlink(filename) < 0)
+		        q_status_message1(SM_ORDER, 0, 2, 
+			  _("Could not remove public certificate %s.crt"), MASTERNAME);
+		      if(rmdir(pathdir) < 0)
+		        q_status_message1(SM_ORDER, 0, 2, 
+		        _("Could not remove temporary directory %s"), pathdir);
+		    }
 		  }
 		}
 	      }

commit 68ac2c9a0ed6d3b0aa1f14c1b5efc9a44e9d871f

* Forwarding did not append the (fwd) suffix to the subject. Bug introduced      in commit 8167f58e118afa658590253f8696c816511c3708 while fixing warnings      given by gcc-10. Reported by Holger Trapp.

pith/pine.hlp
@@ -140,7 +140,7 @@ with help text for the config screen and the composer that didn't have any
 reasonable place to be called from.
 Dummy change to get revision in pine.hlp
 ============= h_revision =================
-Alpine Commit 586 2021-08-31 23:49:55
+Alpine Commit 587 2021-09-07 08:43:39
 ============= h_news =================
 <HTML>
 <HEAD>

pith/reply.c
@@ -2329,12 +2329,11 @@ forward_subject(ENVELOPE *env, int flags)
 	tmp_20k_buf[SIZEOF_20KBUF-1] = '\0';
 
 	removing_trailing_white_space(tmp_20k_buf);
-	if((l = strlen(tmp_20k_buf)) < 1000 &&
-	   (l < 5 || strcmp(tmp_20k_buf+l-5,"(fwd)"))){
+	l = strlen(tmp_20k_buf);
+	if(l < 5 || strcmp(tmp_20k_buf+l-5,"(fwd)")){
 	    char *s = cpystr(tmp_20k_buf);
 	    snprintf(tmp_20k_buf, SIZEOF_20KBUF, "%.1000s (fwd)", s);
 	    tmp_20k_buf[SIZEOF_20KBUF-1] = '\0';
-	    strcpy(tmp_20k_buf, s);
 	    fs_give((void **) &s);
 	}
 