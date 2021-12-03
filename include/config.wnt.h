a4717516707498544201c2e03286142c9abb2539
Eduardo Chappa <chappa@washington.edu>
2021-12-02 21:11:43 -0700

    * New version 2.25.1

--- include/config.wnt.h
+++ include/config.wnt.h
@@ -466,13 +466,13 @@
 #define PACKAGE_NAME "alpine"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "alpine 2.25"
+#define PACKAGE_STRING "alpine 2.25.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "alpine"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.25"
+#define PACKAGE_VERSION "2.25.1"
 
 /* Program users use to change their password */
 /* #undef PASSWD_PROG */
@@ -598,7 +598,7 @@
 #define UTF8_INTERNAL
 
 /* Version number of package */
-#define VERSION "2.25"
+#define VERSION "2.25.1"
 
 /* Windows is just too different */
 #ifndef _WINDOWS
