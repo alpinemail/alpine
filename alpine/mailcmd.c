alpinemail/alpine

commit f95e2d179774739a885468144e68de047245ada5

* If kerberos is not fully installed remove it from the list of libraries.

pith/pine.hlp
@@ -147,7 +147,7 @@ with help text for the config screen and the composer that didn't have any
 reasonable place to be called from.
 Dummy change to get revision in pine.hlp
 ============= h_revision =================
-Alpine Commit 649 2022-06-02 18:13:05
+Alpine Commit 650 2022-06-04 01:28:23
 ============= h_news =================
 <HTML>
 <HEAD>

configure
@@ -20266,6 +20266,7 @@ C_CLIENT_BUNDLED=$alpine_c_client_bundled
 
 
 if test "x$alpine_GSSTYPE" != "xnone" ; then
+  OLDLIB="$LIBS"
   { $as_echo "$as_me:${as_lineno-$LINENO}: checking for library containing gss_init_sec_context" >&5
 $as_echo_n "checking for library containing gss_init_sec_context... " >&6; }
 if ${ac_cv_search_gss_init_sec_context+:} false; then :
@@ -20329,6 +20330,7 @@ else
 	  if test ! -d /usr/kerberos/include ; then
 	    alpine_GSSTYPE="none"
 	    alpine_gss_none_reason="header files not found"
+	    LIBS="$OLDLIBS"
 	  fi
 
 fi

configure.ac
@@ -1684,6 +1684,7 @@ AC_SUBST([C_CLIENT_BUNDLED], $alpine_c_client_bundled)
 
 dnl provide KRB5 support?
 if test "x$alpine_GSSTYPE" != "xnone" ; then
+  OLDLIB="$LIBS"
   AC_SEARCH_LIBS(gss_init_sec_context,gss gssapi gssapi_krb5,
     [
       dnl MIT-based?
@@ -1693,6 +1694,7 @@ if test "x$alpine_GSSTYPE" != "xnone" ; then
 	  if test ! -d /usr/kerberos/include ; then
 	    alpine_GSSTYPE="none"
 	    alpine_gss_none_reason="header files not found"
+	    LIBS="$OLDLIBS"
 	  fi
 	])
     ],

commit 768042b955ae35df6234586a9ea2949f42216248

* New version 2.26
   * If kerberos is not fully installed remove it from the list of libraries.

   * Crash in PC-Alpine when postponing a message. Reported by Paolo Ienne.

   * Cleaning up some memory leaks reported by Valgrind. Memory leaks in the
     handling of http headers will be handled through a different update.

   * Update to reflect the change from alpine.x10host.com to alpineapp.email.

   * Fix regression introduced by f95e2d179774739a885468144e68de047245ada5:

     When testing for Kerberos support consistently use OLDLIBS as the
     variable to temporarily save LIBS to, i.e., fix the mis-spelling of

   * Crash in Alpine after a user tried to repaint the screen after failing
     to select a rule from a menu. Reported by Peter Tirsek. Crash also
     reproducible when selecting by keyword or charset.

alpine/mailcmd.c
@@ -8952,6 +8952,7 @@ choose_a_rule(int rflags)
     int        cnt = 0;
     PAT_S     *pat;
     PAT_STATE  pstate;
+    void (*redraw)(void) = ps_global->redrawer;
 
     if(!(nonempty_patterns(rflags, &pstate) && first_pattern(&pstate))){
 	q_status_message(SM_ORDER, 3, 3,
@@ -8985,8 +8986,10 @@ choose_a_rule(int rflags)
 				   _("rules"), h_select_rule_screen,
 				   _("HELP FOR SELECTING A RULE NICKNAME"), NULL);
 
-    if(!choice)
+    if(!choice){
       q_status_message(SM_ORDER, 1, 4, "No choice");
+      ps_global->redrawer = redraw;
+    }
 
     free_list_array(&rule_list);
 
@@ -9169,6 +9172,7 @@ choose_a_keyword(void)
     char     **keyword_list, **lp;
     int        cnt;
     KEYWORD_S *kw;
+    void (*redraw)(void) = ps_global->redrawer;
 
     /*
      * Build a list of keywords to choose from.
@@ -9196,8 +9200,10 @@ choose_a_keyword(void)
 				   _("keywords"), h_select_keyword_screen,
 				   _("HELP FOR SELECTING A KEYWORD"), NULL);
 
-    if(!choice)
+    if(!choice){
       q_status_message(SM_ORDER, 1, 4, "No choice");
+      ps_global->redrawer = redraw;
+    }
 
     free_list_array(&keyword_list);
 
@@ -9276,6 +9282,7 @@ choose_a_charset(int which_charsets)
     char     **charset_list, **lp;
     const CHARSET *cs;
     int        cnt;
+    void (*redraw)(void) = ps_global->redrawer;
 
     /*
      * Build a list of charsets to choose from.
@@ -9319,8 +9326,10 @@ choose_a_charset(int which_charsets)
 				   _("character sets"), h_select_charset_screen,
 				   _("HELP FOR SELECTING A CHARACTER SET"), NULL);
 
-    if(!choice)
+    if(!choice){
       q_status_message(SM_ORDER, 1, 4, "No choice");
+      ps_global->redrawer = redraw;
+    }
 
     free_list_array(&charset_list);
 

pith/pine.hlp
@@ -147,7 +147,7 @@ with help text for the config screen and the composer that didn't have any
 reasonable place to be called from.
 Dummy change to get revision in pine.hlp
 ============= h_revision =================
-Alpine Commit 661 2022-09-03 07:46:35
+Alpine Commit 662 2022-09-08 22:05:03
 ============= h_news =================
 <HTML>
 <HEAD>
@@ -205,6 +205,9 @@ new additions to Alpine, please check it periodically.
 <P> Bugs addressed include:
 <UL>
 <LI> Crash in PC-Alpine when postponing a message. Reported by Paolo Ienne.
+<LI> Crash in Alpine after a user tried to repaint the screen after failing
+     to select a rule from a menu. Reported by Peter Tirsek. Crash also 
+     reproducible when selecting by keyword or charset.
 </UL>
 
 <P>Version 2.26 adds new features and addresses bugs found in previous